08971c94960597207d81bcd9739766d60f883485
Wender de Oliveira <60203057+wender95@users.noreply.github.com>
2023-11-14 16:16:26 -0300

    Update scripts.js

--- clientes/CAD/js/scripts.js
+++ clientes/CAD/js/scripts.js
@@ -4,9 +4,9 @@ function enviarFormulario() {
     const telefone = document.getElementById('telefone').value;
     const endereco = document.getElementById('endereco').value;
 
-    // Verifique se todos os campos foram preenchidos antes de enviar
+   
     if (nome && cpf && telefone && endereco) {
-        // Enviar dados para o servidor (substitua a URL pelo seu endpoint)
+     
         fetch('/salvar-dados', {
             method: 'POST',
             headers: {
@@ -16,12 +16,12 @@ function enviarFormulario() {
         })
         .then(response => response.json())
         .then(data => {
-            // Redirecionar para a página de agradecimento
+           
             window.location.href = '/agradecimento';
         })
         .catch(error => console.error('Erro ao enviar dados:', error));
     } else {
-        // Exiba uma mensagem de erro se algum campo estiver em branco
+        
         document.getElementById('mensagem').innerText = 'Preencha todos os campos antes de enviar.';
     }
 }
